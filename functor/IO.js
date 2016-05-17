693998e7c9f7bf86eaacf505ca9e6d21e85d5ded
892280082 <892280082@qq.com>
2016-05-17 22:27:20 +0800

    ok

---
+++ functor/IO.js
@@ -0,0 +1,25 @@
+var _ = require('ramda');
+
+var IO = function(f){
+	this.__value = f;
+};
+
+IO.of = function(x){
+	return new IO(function(){
+		return x;
+	});
+};
+
+IO.prototype.map = function(f){
+	return new IO(_.compose(f,this.__value));
+};
+
+IO.prototype.join = function(){
+	return this.unsafePerformIO();
+};
+
+/**
+ * Module exports.
+ * @public
+ */
+module.exports = IO;
